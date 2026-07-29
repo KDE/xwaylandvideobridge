0f7ed9fdcfa2502d539eb7acf8232b127a219673
Hadi Chokr <hadichokr@icloud.com>
2026-07-29 10:42:47 +0200

    Bump version to 0.5.1
    BUG: 522597
    FIXED-IN: 0.5.1

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
             }:
             stdenv.mkDerivation {
               pname = "xwaylandvideobridge";
-              version = "0.5.0-kf6";
+              version = "0.5.1-kf6";
               src = ./.;
               nativeBuildInputs = [
                 cmake
