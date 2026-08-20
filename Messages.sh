077314272296839954b7afd29a706368ca2edfb7
Hadi Chokr <hadichokr@icloud.com>
2026-08-20 14:07:39 +0200

    Fix Messages.sh.
    Signed-off-by: Hadi Chokr <hadichokr@icloud.com>

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: 2023 Aleix Pol <aleixpol@kde.org>
+# SPDX-FileCopyrightText: 2026 Hadi Chokr <hadichokr@icloud.com>
 
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/xwaylandvideobridge.pot
+$EXTRACTRC `find src -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find src -name \*.cpp -o -name \*.h -o -name \*.qml` rc.cpp -o $podir/xwaylandvideobridge.pot
 rm -f rc.cpp
